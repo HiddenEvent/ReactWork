aa9ebd629288f771c3b38f71750c51fc79327bd0
RichardKim <hiddeneventor@gmail.com>
2021-05-23 14:14:04 +0900

    useEffect 테스트

--- voca/src/component/DayList.js
+++ voca/src/component/DayList.js
@@ -1,18 +1,37 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import dummy from '../db/data.json';
 
 const DayList = () => {
-  console.log(dummy);
+  const [days, setDays] = useState([]);
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    console.log('change Count');
+  }, []);
+
+  function countClick() {
+    setCount(count + 1);
+  }
+  function countClick2() {
+    setDays([
+      ...days,
+      {
+        id: Math.random(),
+        day: 1,
+      },
+    ]);
+  }
 
   return (
     <ul className="list_day">
-      {dummy.days.map((day) => (
+      {days.map((day) => (
         <li key={day.id}>
           <Link to={`/day/${day.day}`}>Day {day.day}</Link>
         </li>
       ))}
-      <li></li>
+      <button onClick={countClick2}>DayChange</button>
+      <button onClick={countClick}>{count}</button>
     </ul>
   );
 };
