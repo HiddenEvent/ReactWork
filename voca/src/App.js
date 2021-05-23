ada46d904cf8d5fb93818a7c62ad500603a4e194
RichardKim <hiddeneventor@gmail.com>
2021-05-23 11:04:28 +0900

    React Hooks : useState 연습

--- voca/src/App.js
+++ voca/src/App.js
@@ -8,6 +8,8 @@ function App() {
   return (
     <div className="App">
       <Hellow />
+      <Hellow />
+      <Hellow />
       <Bottom />
     </div>
   );
--- voca/src/component/Hellow.js
+++ voca/src/component/Hellow.js
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './Hellow.module.css';
 
 const Hellow = () => {
+  let [name, setName] = useState('Mike');
+
   return (
     <div>
       <h1>자 컴포넌트 간다</h1>
       <p className={styles.box}>css 테스트</p>
+
+      <h2>{name}</h2>
+      <button
+        onClick={() => {
+          setName(name === 'Mike' ? 'Jane' : 'Mike');
+        }}
+      >
+        change
+      </button>
     </div>
   );
 };
