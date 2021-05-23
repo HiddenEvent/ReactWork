d7934b45577f06b4122444195c1f555bb170553b
RichardKim <hiddeneventor@gmail.com>
2021-05-23 12:54:44 +0900

    오타 수정

--- voca/src/component/Header.js
+++ voca/src/component/Header.js
@@ -5,7 +5,7 @@ const Header = () => {
   return (
     <div className="header">
       <h1>
-        <Link href="/">토익 영단어(고급)</Link>
+        <Link to="/">토익 영단어(고급)</Link>
       </h1>
       <div className="menu">
         <Link to="/">단어 추가</Link>
