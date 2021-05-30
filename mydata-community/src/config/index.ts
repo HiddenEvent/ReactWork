7a916533e6a582e3d4e8c21212fedd61d5ce40a8
RichardKim <hiddeneventor@gmail.com>
2021-05-31 01:02:00 +0900

    UI 설계중

--- mydata-community/src/config/index.ts
+++ mydata-community/src/config/index.ts
@@ -20,69 +20,69 @@ import RouteItem from '../model/RouteItem.model';
 
 // define app routes
 export const routes: Array<RouteItem> = [
-    {
-        key: "router-home",
-        title: "Home",
-        tooltip: "Home",
-        path: "/",
+  {
+    key: 'router-home',
+    title: '홈',
+    tooltip: '스윗홈',
+    path: '/',
+    enabled: true,
+    component: Home,
+    icon: HomeIcon,
+    appendDivider: true,
+  },
+  {
+    key: 'router-dashboard',
+    title: '기출문제',
+    tooltip: '기출문제',
+    path: '/dashboard',
+    enabled: true,
+    component: Dashboard,
+    icon: DashboardIcon,
+  },
+  {
+    key: 'router-gh',
+    title: '함께해요',
+    tooltip: '나와함께',
+    enabled: true,
+    icon: GitHubIcon,
+    subRoutes: [
+      {
+        key: 'router-gh-private',
+        title: '비공개 자료',
+        tooltip: '비공개 자료',
+        path: '/gh/private',
         enabled: true,
-        component: Home,
-        icon: HomeIcon,
-        appendDivider: true
-    },
-    {
-        key: "router-dashboard",
-        title: "Dashboard",
-        tooltip: "Dashboard",
-        path: "/dashboard",
-        enabled: true,
-        component: Dashboard,
-        icon: DashboardIcon
-    },
-    {
-        key: "router-gh",
-        title: "GitHub",
-        tooltip: "GitHub",
-        enabled: true,
-        icon: GitHubIcon,
-        subRoutes: [
-            {
-                key: "router-gh-private",
-                title: "Private Repos",
-                tooltip: "Private Repos",
-                path: "/gh/private",
-                enabled: true,
-                component: GHPrivate,
-                icon: PrivateIcon
-            }
-            , {
-                key: "router-gh-public",
-                title: "Public Repos",
-                tooltip: "Public Repos",
-                path: "/gh/public",
-                enabled: false,
-                component: GHPublic,
-                icon: PublicIcon
-            }
-        ]
-    },
-    {
-        key: "router-code",
-        title: "Code Editor",
-        tooltip: "Code Editor",
-        path: "/code-editor",
-        enabled: true,
-        component: CodeEditor,
-        icon: CodeIcon,
-        appendDivider: true
-    },
-    {
-        key: "router-settings",
-        title: "Settings",
-        tooltip: "Settings",
-        path: "/settings",
-        enabled: true,
-        component: Settings,
-        icon: SettingsIcon
-    },
-]
+        component: GHPrivate,
+        icon: PrivateIcon,
+      },
+      {
+        key: 'router-gh-public',
+        title: '공개 자료',
+        tooltip: '공개 자료',
+        path: '/gh/public',
+        enabled: false,
+        component: GHPublic,
+        icon: PublicIcon,
+      },
+    ],
+  },
+  {
+    key: 'router-code',
+    title: '마이데이터 공부방',
+    tooltip: '마이데이터 용어집',
+    path: '/code-editor',
+    enabled: true,
+    component: CodeEditor,
+    icon: CodeIcon,
+    appendDivider: true,
+  },
+  {
+    key: 'router-settings',
+    title: 'Settings',
+    tooltip: 'Settings',
+    path: '/settings',
+    enabled: true,
+    component: Settings,
+    icon: SettingsIcon,
+  },
+];
